74362820738a55ceb7c6a4fa46253c5ff7c90c08
Tarun Kumar <tarun.k@toppr.com>
2020-08-10 13:45:09 +0530

    GET-API Slots

--- aveo_backend/availability/urls.py
+++ aveo_backend/availability/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("ta/api/teacher/", views.get_teachers_list, name="get-teacher_list"),
     path("ta/api/teacher/add/", views.add_teacher, name="add-teacher"),
     path("ta/api/teacher/<int:pk>/", views.get_teacher, name="get-teacher"),
+    path("<int:pk>/", views.test_view, name='tes-view')
 ]

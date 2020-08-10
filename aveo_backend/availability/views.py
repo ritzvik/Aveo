dfe26c85eca5b735076f449fe7f9a23dadef7278
Tarun Kumar <tarun.k@toppr.com>
2020-08-10 12:09:05 +0530

    Basic APIs

--- aveo_backend/availability/views.py
+++ aveo_backend/availability/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TeacherSerializer
+from .models import Teacher
 
-# Create your views here.
+
+# APIs For teachers table
+@api_view(["GET"])
+def get_teachers_list(request):
+    teacher = Teacher.objects.all()
+    serializer = TeacherSerializer(teacher, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def add_teacher(request):
+    serializer = TeacherSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def get_teacher(request, pk):
+    teacher = Teacher.objects.get(id=pk)
+    serializer = TeacherSerializer(teacher, many=False)
+    return Response(serializer.data)
+
+

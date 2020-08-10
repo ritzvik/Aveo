71b0e54db1d74bf4a25ff161993b43f61328355e
Ritwik Saha <s.ritwik98@gmail.com>
2020-08-10 18:28:28 +0530

    Initiate a simple test

--- aveo_backend/availability/tests.py
+++ aveo_backend/availability/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Teacher, ValidSlot
 
 # Create your tests here.
+
+
+class TeacherTestCase(TestCase):
+    def setUp(self):
+        Teacher.objects.create(first_name="Testy", last_name="McTestFace")
+
+    def test_entry_is_there(self):
+        try:
+            obj = Teacher.objects.get(first_name="Testy", last_name="McTestFace")
+            assert True
+        except Teacher.DoesNotExist as err:
+            print(err)
+            assert False

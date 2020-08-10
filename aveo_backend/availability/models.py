546a2440f6d09afdfdc2c371c180e869df8ca4c0
Ritwik Saha <s.ritwik98@gmail.com>
2020-08-10 10:31:42 +0530

    add models for slots

--- aveo_backend/availability/models.py
+++ aveo_backend/availability/models.py
@@ -5,3 +5,17 @@ class Teacher(models.Model):
     id = models.AutoField(primary_key=True)
     first_name = models.TextField()
     last_name = models.TextField()
+
+
+class ValidSlot(models.Model):
+    id = models.AutoField(primary_key=True)
+    day = models.IntegerField()  # 0 for monday, 1 for tuesday .... 6 for sunday
+    start_time = models.TimeField()  # slots are assumed to be of 1 hour, so no end_time
+
+
+class AvailableSlot(models.Model):
+    id = models.AutoField(primary_key=True)
+    teacher_id = models.ForeignKey("Teacher", on_delete=models.SET_NULL, null=True)
+    validslot_id = models.ForeignKey("ValidSlot", on_delete=models.SET_NULL, null=True)
+    date = models.DateField()
+    status = models.IntegerField()  # 0 for cancelled, 1 for available, 2 for confirmed

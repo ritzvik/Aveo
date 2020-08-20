8264ed9bbc6891537ba381522f4c8534b1f44da0
Tarun Kumar <tarun12.tarunkr@gmail.com>
2020-08-21 02:09:02 +0530

    Loader

--- aveo_frontend/src/components/SlotUnit.js
+++ aveo_frontend/src/components/SlotUnit.js
@@ -1,5 +1,5 @@
 import {css, StyleSheet} from "aphrodite";
-import React, {useState} from "react";
+import React from "react";
 import {Container} from "react-bootstrap";
 
 const styles = StyleSheet.create({
